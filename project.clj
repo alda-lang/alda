6e1649d8fb5633db167037a51f0bcced248ed8ff
Dave Yarwood <dave.yarwood@gmail.com>
2013-09-14 18:30:49 -0400

    Added skeleton created by Leiningen, plus a workbench with some stuff to work on.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject yggdrasil "0.1.0-SNAPSHOT"
+  :description "A music programming language for musicians"
+  :url "http://www.none-yet.com"
+  :license {:name "Eclipse Public License"
+  		     :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ test/yggdrasil/core_test.clj
@@ -0,0 +1,7 @@
+(ns yggdrasil.core-test
+  (:require [clojure.test :refer :all]
+            [yggdrasil.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
