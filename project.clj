29e271046b5f9a827a978ca083bd978aa98e54e8
Dave Yarwood <dave.yarwood@gmail.com>
2013-09-18 04:39:52 +0000

    project.clj edited online with Bitbucket

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
   		     :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                                  [org.clojure/tools.cli "0.2.4"]]
+                 [org.clojure/tools.cli "0.2.4"]]
   :main yggdrasil.core)
