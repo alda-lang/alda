b9ff38c589ea86c87643cc5166ca8a7e2bee7f9c
Dave Yarwood <dave.yarwood@gmail.com>
2013-09-18 00:31:51 -0400

    Added a core.clj skeleton with some command line argument parsing.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://www.none-yet.com"
   :license {:name "Eclipse Public License"
   		     :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                                  [org.clojure/tools.cli "0.2.4"]]
+  :main yggdrasil.core)
--- src/yggdrasil/core.clj
+++ src/yggdrasil/core.clj
@@ -1,5 +1,40 @@
-(ns yggdrasil.core)
+(ns yggdrasil.core
+	(:use [clojure.tools.cli :only (cli)])
+	(:gen-class :main true))
 
-; this file will parse command line arguments and call the appropriate functions
-
-
+(defn -main
+	"Parses command line arguments and calls the appropriate functions."
+	[& args]
+	(let [[opts args banner]
+		(cli args
+			["-h" "--help" "Display help text." :flag true, :default false]
+			["-s" "--start" "Start playing the score at a specific minute/second mark." :default 0:00]
+			["-e" "--end" "Stop playing at a specific minute/second mark in the score."] ;; optional
+			)]
+	   (when (:help opts)
+	   	   (println banner)
+	   	   (System/exit 0))
+	   (cond 
+	   	   (zero? (count args)) (comment 
+	   	   	   "Error message about needing to specify a .yg file, and exit.")
+	   	   (> 2 (count args)) (comment
+	   	   	   "Error message about > 2 arguments. Remind user of syntax and exit."))	   
+	   (let [wav-data (comment 
+	   		   "Code for parsing the .yg file, which should be (first args).
+	   		   Should first check that the file exists and return an error message if
+	   		   the file is not found.
+	   		   This should return a working wav-data object of some sort?")]
+	      (if (= 2 (count args)) 
+	      	  (comment 
+	      	  	  "Code that checks (last args), which should be the destination .wav file name,
+	      	  	  and sees if that file already exists in the current working directory. 
+	      	  	  If so, prompts the user if he/she wants to overwrite the file.
+	      	  	  Once a good file name is established, writes wav-data to the file, prints 
+	      	  	  something letting the user know it has done so, then exits happily.")
+	      	  (comment
+	      	  	  "Code that plays the wav-data. Short term goal is just to get this working.
+	      	  	  I'd eventually like to implement a slick CLI music player display that shows
+	      	  	  how long the music is in minutes/seconds, how far into it you are with a
+	      	  	  progress bar, and so on. Maybe even listen for keyboard input and allow
+	      	  	  the user to pause, stop, restart, jump to a specific time marking or named
+	      	  	  marker, etc. Could eventually expand this into a full-blown GUI.")))))
