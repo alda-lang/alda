0020a83a4e541df1842aa71cb39e8df9237b3097
Dave Yarwood <dave.yarwood@gmail.com>
2013-10-18 23:56:05 -0400

    Updated parser.clj

--- src/yggdrasil/parser.clj
+++ src/yggdrasil/parser.clj
@@ -52,6 +52,87 @@
 
   (insta/parser "grammar/separate-instruments.txt"))
 
+(defn nicknames
+  "Takes the output of the separate-instruments parser and returns a map of
+   nicknames to the instrument(s) to which they refer."
+   [parse-tree]
+   (letfn [(has-nickname? [instrument]
+              (->> (second instrument)
+                   (rest) 
+                   (some #(= (first %) :nickname))))]
+     (->> (rest parse-tree)
+          (filter has-nickname?)
+          (map (fn [instrument]
+                 (let [nickname    (->> (second instrument)
+                                        (rest)
+                                        (last)
+                                        (last))
+                       instruments (->> (second instrument)
+                                        (rest)
+                                        (drop-last)
+                                        (map last))]
+                   {nickname instruments})))
+          (apply merge))))
+
+(defn expand-nicknames
+  "Takes the output of the separate-instruments parser and transforms it by
+   expanding nicknamed groups/instruments, returning a more organized parse tree
+   consisting of each instrument-call and its respective music data.
+   (This does not consolidate repeated instrument calls.)"
+  [parse-tree]
+  (let [nicknames (nicknames parse-tree)
+
+        expand (fn expand [x]
+                 (cond 
+                   (not (coll? (first x)))  ; x is a single node
+                   (let [type (first x)
+                         name (last x)]
+                     (cond
+                       (= type :nickname)
+                       nil
+
+                       (contains? nicknames name)
+                       (expand (map #(vector :name %) (nicknames name)))
+
+                       :else
+                       x))
+
+                   :else  ; x is a seq of nodes
+                   (->> (map expand x)
+                        (remove nil?))))       
+
+        expand-and-tag (fn [name-nodes]
+                         (->> (expand name-nodes)
+                              (mapcat #(if (coll? (first %)) % [%]))
+                              (cons :instrument-call)
+                              (apply vector)))]
+
+    (insta/transform {:instrument-call expand-and-tag} parse-tree)))
+
+(defn consolidate-instruments
+  "Takes the output of separate-instruments parser -> expand-nicknames and 
+   consolidates repeated calls to the same instrument, returning an even more
+   organized parse tree consisting of each individual instrument and its respective
+   music data.
+
+   e.g.
+  [:score
+    [:instrument
+      [:name 'cello']
+      [:music-data 'cello music']]
+    [:instrument
+      [:name 'trumpet']
+      [:music-data 'brass music']]
+    [:instrument
+      [:name 'trombone']
+      [:music-data 'brass music']]
+    [:instrument
+      [:name 'tuba']
+      [:music-data 'brass music']]]"
+
+  [parse-tree]
+  (comment "To do."))
+
 (def parse-ygg-code
   "Takes a string of music-data (for one instrument) and returns a parse tree of music 
    events, including attribute changes and notes."
