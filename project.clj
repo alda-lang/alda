ee176a5fc1a2d62a3372602377353d9e7e8e7feb
Dave Yarwood <dave.yarwood@gmail.com>
2014-08-12 21:26:44 -0400

    update instaparse dep to 1.3.3

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.4"]
-                 [instaparse "1.2.3"]]
+                 [instaparse "1.3.3"]]
   :main yggdrasil.core)
