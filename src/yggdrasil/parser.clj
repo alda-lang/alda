f27275a178d14521a35dca7e2700c5439e6b2976
Dave Yarwood <dave.yarwood@gmail.com>
2014-08-26 00:06:05 -0400

    simplify parser.clj with a single parser

--- src/yggdrasil/parser.clj
+++ src/yggdrasil/parser.clj
@@ -3,67 +3,40 @@
             [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(comment
-  "STEP ONE:
-   The .yg file is fed through strip-comments to remove comments and barlines,
-   then the result of that is fed to separate instruments, which returns a simple
-   parse-tree with this kind of structure:
+(declare assign-instances consolidate-instruments)
 
-   [:score
-     [:instrument
-       [:instrument-call
-         [:name 'cello']]
-       [:music-data 'string of ygg code for the cello']]
-     [:instrument
-       [:instrument-call
-         [:name 'trumpet']
-         [:name 'trombone']
-         [:name 'tuba']
-         [:nickname 'brass']]
-       [:music-data 'string of ygg code for the brass group']]]    ")
+(def ygg-parser
+  (insta/parser (io/resource "grammar/yggdrasil.bnf")))
 
-(defn- strip-comments [score]
-  "Takes a string of ygg code, returns a string with comments/barlines removed."
-  (->> score
-    (insta/parse (insta/parser (io/resource "grammar/strip-comments.bnf")))
-    (insta/transform {:score str})))
+(defn parse-input
+  "Parses a string of ygg code, determines which instrument instances are assigned to
+   each instrument call, and returns a map of each instrument instance to its own parse
+   tree of music data."
+  [ygg-code]
+  (->> ygg-code
+       ygg-parser
+       (insta/transform {:score assign-instances})
+       (insta/transform {:score consolidate-instruments})))
 
-(defn- separate-instruments [score]
-  "Takes a string of de-commented ygg code, returns a parse tree."
-  (->> score
-    (insta/parse (insta/parser 
-                   (io/resource "grammar/separate-instruments.bnf")))
-    (insta/transform {:music-data (fn [& chars]
-    		                            [:music-data (str/join chars)])})))
+(defn- assign-instances
+  "Reconstructs the parse tree by going through the score linearly and deducing what
+   specific instrument 'instances' are referred to by each instrument call. In the
+   resulting parse tree, each :instrument node has a :tracks node that lists these
+   instrument instances, e.g. [:tracks {'guitar' 1} {'bass' 1}], rather than an
+   :instrument-call node.
 
-(comment
-  "STEP TWO:
-   Before going to the next parser, the result of Step 1 is transformed by two
-   functions. The first function, assign-instances, goes through the score linearly
-   and deduces what specific instrument 'instances' are referred to by each
-   instrument call. The resulting parse-tree is different in that each :instrument
-   node has a :tracks node instead of an :instrument-call node. The :tracks node
-   indicates which instrument instances are being called,
-   e.g. [:tracks {'guitar' 1} {'bass' 1}].
+   A new numbered instance of an instrument is created whenever a stock instrument is
+   called with a nickname, either as part of a group or not.
 
-   (NOTE: A new numbered instance of an instrument is created whenever a stock
-          instrument is called with a nickname, either as part of a group or not.
+   e.g. If there is already a clarinet 1 and there is already a cello 1 nicknamed
+   'thor', then this -- thor/clarinet 'band': -- will refer to the same instance of
+   cello (cello 1, 'thor'), but a new clarinet instance (clarinet 2) because a nickname,
+   'band' is being given to this group, and 'clarinet' refers to the stock instrument,
+   not any particular named instance of clarinet.
 
-          If there is already a clarinet(1) and there is already a cello(1)
-          nicknamed 'thor', then calling -- thor/clarinet 'band': -- will refer to
-          the same instance of cello (1, thor), but a *new* instance of clarinet(2)
-          because a nickname, 'band', is being given to the group, and 'clarinet'
-          refers to the stock instrument, not any particular named instance of
-          clarinet.
-
-          On the other hand, calling -- thor/clarinet: -- in the same scenario will
-          refer to cello(1) and clarinet(1), the same instances that were already
-          'in play,' so to speak.)
-
-   The second function consolidates repeated calls to the same instrument instance
-   and returns a simple map of each instance to its consolidated music data.")
-
-(defn- assign-instances [[_ & instrument-nodes :as parse-tree]]
+   On the other hand, -- thor/clarinet: -- in the same scenario would refer to cello 1
+   and clarinet 1, the same instances that were already in use."
+  [& instrument-nodes]
   (loop [table {}, ; a map of the "names" to the instrument instance(s)
                    ; e.g. {"bill" [{"cello" 1}], "bob" [{"cello" 2}], "trumpet" [{"trumpet" 1}],
                    ;       "brass" [{"trumpet" 1} {"trombone" 1} {"tuba" 1}]}
@@ -75,10 +48,9 @@
          nodes instrument-nodes]
     (if-not (seq nodes)
       score
-      (let [[_ [_ & name-nodes] music-data-node :as instrument-node] (first nodes)
+      (let [[_ [_ & name-nodes] music-data-node] (first nodes)
 
-            nickname
-            ; returns the nickname of the node or nil
+            nickname ; returns the nickname of the node or nil
             (last (last (filter #(= (first %) :nickname) name-nodes)))
 
             assign
@@ -121,42 +93,46 @@
 
         (recur updated-table updated-nicknames updated-score (rest nodes))))))
 
-(defn- consolidate-instruments [[_ & instrument-nodes :as parse-tree]]
-  "Returns a map of instrument instances to their consolidated music-data."
-  	(letfn [(add-music-data
-  			      [score ; map
-  			      [_ [_ & instances] [_ music-data]]] ; instrument-node
-  			      (reduce (fn [m instance]
-  			      		      (merge-with #(str % " " %2) m {instance music-data}))
+(defn- consolidate-instruments
+  "Returns a map of instrument instances to their consolidated music data."
+  [& instrument-nodes]
+  	(letfn [(add-music-data [score ; map
+  			                    [_ [_ & instances] [_ & events]]] ; instrument node
+  			      (reduce (fn [m instance] (merge-with concat m {instance events}))
   			               score
   			               instances))]
   		(reduce add-music-data {} instrument-nodes)))
 
 (comment
-  "STEP THREE:
-  Each instance's music-data goes through the parse-ygg-code parser separately.
-  The music-data is a string of ygg-code representing all of what that instrument
-  will do for the duration of the score. The parser parses the music-data and
-  returns a parse-tree of music events, in linear order. Music events are
-  optionally grouped by 'marker' nodes. Simultaneous events can be grouped (within
-  a list of linear music events) into 'chord' nodes.
+  "Each instrument now has its own vector of music events, representing everything that instrument
+   will do for the duration of the score.
 
-   At this point, will probably hand off the final parse trees (one per instrument
-   or instrument group) to yggdrasil.generator, which will hopefully be able to
-   create audio segments of each instrument at each marker, assigning time
-   markings where each one starts (or perhaps for every single music event), and
-   then use the time markings to layer all the different audio segments together to
-   create the final audio file.")
+   To do:
 
-(def parse-ygg-code
-  (insta/parser (io/resource "grammar/parse-ygg-code.bnf")))
+     - Implement a way to embed time markers of some sort among the musical events,
+       so that they end up synchronized between the musical instruments. Each instrument
+       by default starts its musical events at '0', i.e. the beginning, but the composer
+       will be able to specify where a musical event will fall by using markers.
 
+         Special cases:
+           * Chords: each note in the chord starts at the same time mark. The next event
+                     after the chord will be assigned that time mark + the duration of the
+                     longest note/rest in the chord.
+           * Voices: voices work just like chords. Each voice in the voices grouping
+                     starts at the same time mark. The next event after the voices grouping
+                     (whether via explicit V0: or switching back from another instrument)
+                     will start whenever the last voice has finished its music data...
+                     i.e. the 'largest' time marking out of all the voices.
 
-; example -- it's working so far!
-(->> (slurp "test/yggdrasil/awobmolg.yg")
-     (strip-comments)
-     (separate-instruments)
-     (assign-instances)
-     (consolidate-instruments))
+                     Implications: the composer will need to either make use of named markers
+                     or make sure, when switching from instrument to instrument, that he be
+                     aware that the end of the longest held voice is where the next event
+                     will come in after switching back to that instrument. I don't really
+                     foresee this as being an inconvience. Markers will be easy to use.
 
+     -  At this point, will probably hand off the final parse trees (one per instrument)
+        to yggdrasil.sound_generator, which will hopefully be able to create audio segments
+        of each instrument at each time marking, and then use the time markings to layer all
+        the different audio segments together to create the final audio file.")
 
+(parse-input (slurp "test/yggdrasil/awobmolg.yg"))
