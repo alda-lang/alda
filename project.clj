d34b71956ea2f6f336629de664a3dc11c8699a02
Dave Yarwood <dave.yarwood@gmail.com>
2014-08-26 00:13:51 -0400

    Add github link to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject yggdrasil "0.1.0"
   :description "A music programming language for musicians"
-  :url "http://www.none-yet.com"  ; will make a github when ready to go public
+  :url "http://www.github.com/daveyarwood/yggdrasil"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
