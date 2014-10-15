4f105296ee3f3df8d0936c11e6dc636a836c0dbd
Dave Yarwood <dave.yarwood@gmail.com>
2014-10-14 22:51:11 -0400

    use instaparse's failure? function

--- test/yggdrasil/parser_test.clj
+++ test/yggdrasil/parser_test.clj
@@ -1,15 +1,16 @@
 (ns yggdrasil.parser-test
   (:require [clojure.test :refer :all]
             [clojure.pprint :refer :all]
-            [yggdrasil.parser :refer :all]))
+            [yggdrasil.parser :refer :all]
+            [instaparse.core :as insta]))
 
 (deftest parser-test
   (testing "parsing valid input"
     (testing "all watched over by machines of loving grace"
       (let [result (parse-input (slurp "test/yggdrasil/awobmolg.yd"))]
-        (is (not (instance? instaparse.gll.Failure result)))
+        (is (not (insta/failure? result)))
         (pprint result)))
     (testing "debussy string quartet"
       (let [result (parse-input (slurp "test/yggdrasil/debussy_quartet.yd"))]
-        (is (not (instance? instaparse.gll.Failure result)))
+        (is (not (insta/failure? result)))
         (pprint result)))))
