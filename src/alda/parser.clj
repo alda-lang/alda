ed866568369703e1433e506c6f792370e88edb1a
Dave Yarwood <dave.yarwood@gmail.com>
2015-01-19 22:31:16 -0500

    try to be slightly more diligent about 80-character line-length

--- src/alda/parser.clj
+++ src/alda/parser.clj
@@ -9,9 +9,9 @@
   (insta/parser (io/resource "grammar/alda.bnf")))
 
 (defn parse-input
-  "Parses a string of alda code, determines which instrument instances are assigned to
-   each instrument call, and returns a map of each instrument instance to its own parse
-   tree of music data."
+  "Parses a string of alda code, determines which instrument instances are
+   assigned to each instrument call, and returns a map of each instrument
+   instance to its own parse tree of music data."
   [alda-code]
   (->> alda-code
        alda-parser
@@ -34,23 +34,23 @@
 (declare update-data)
 
 (defn- assign-instances
-  "Reconstructs the parse tree by going through the score linearly and deducing what
-   specific instrument 'instances' are referred to by each instrument call. In the
-   resulting parse tree, each :instrument node has a :tracks node that lists these
-   instrument instances, e.g. [:tracks {'guitar' 1} {'bass' 1}], rather than an
-   :instrument-call node.
+  "Reconstructs the parse tree by going through the score linearly and deducing
+   what specific instrument 'instances' are referred to by each instrument call.
+   In the resulting parse tree, each :instrument node has a :tracks node that
+   lists these instrument instances, e.g. [:tracks {'guitar' 1} {'bass' 1}],
+   rather than an :instrument-call node.
 
-   A new numbered instance of an instrument is created whenever a stock instrument is
-   called with a nickname, either as part of a group or not.
+   A new numbered instance of an instrument is created whenever a stock instrument
+   is called with a nickname, either as part of a group or not.
 
    e.g. If there is already a clarinet 1 and there is already a cello 1 nicknamed
-   'thor', then this -- thor/clarinet 'band': -- will refer to the same instance of
-   cello (cello 1, 'thor'), but a new clarinet instance (clarinet 2) because a nickname,
-   'band' is being given to this group, and 'clarinet' refers to the stock instrument,
-   not any particular named instance of clarinet.
+   'thor', then this -- thor/clarinet 'band': -- will refer to the same instance
+   of cello (cello 1, 'thor'), but a new clarinet instance (clarinet 2) because
+   a nickname, 'band' is being given to this group, and 'clarinet' refers to the
+   stock instrument, not any particular named instance of clarinet.
 
-   On the other hand, -- thor/clarinet: -- in the same scenario would refer to cello 1
-   and clarinet 1, the same instances that were already in use."
+   On the other hand, -- thor/clarinet: -- in the same scenario would refer to
+   cello 1 and clarinet 1, the same instances that were already in use."
   [& instrument-nodes]
   (-> (reduce update-data {:table {}, :nicknames {}, :score [:score]}
                           instrument-nodes)
@@ -59,7 +59,8 @@
 (defn- assign
   "Assigns instance(s) to a name-node. Returns nil for nickname nodes.
 
-   nickname is nickname of the current instrument call, if it has one, otherwise nil.
+   nickname is nickname of the current instrument call, if it has one, otherwise
+   nil.
 
    data represents a map of 'working data' including:
 
@@ -118,35 +119,39 @@
   (reduce add-music-data {} instrument-nodes))
 
 (comment
-  "Each instrument now has its own vector of music events, representing everything that instrument
-   will do for the duration of the score.
+  "Each instrument now has its own vector of music events, representing
+   everything that instrument will do for the duration of the score.
 
    To do:
 
-     - Implement a way to embed time markers of some sort among the musical events,
-       so that they end up synchronized between the musical instruments. Each instrument
-       by default starts its musical events at '0', i.e. the beginning, but the composer
-       will be able to specify where a musical event will fall by using markers.
+     - Implement a way to embed time markers of some sort among the musical
+       events, so that they end up synchronized between the musical instruments.
+       Each instrument by default starts its musical events at '0', i.e. the
+       beginning, but the composer will be able to specify where a musical event
+       will fall by using markers.
 
          Special cases:
-           * Chords: each note in the chord starts at the same time mark. The next event
-                     after the chord will be assigned that time mark + the duration of the
-                     longest note/rest in the chord.
-           * Voices: voices work just like chords. Each voice in the voices grouping
-                     starts at the same time mark. The next event after the voices grouping
-                     (whether via explicit V0: or switching back from another instrument)
-                     will start whenever the last voice has finished its music data...
-                     i.e. the 'largest' time marking out of all the voices.
+           * Chords: each note in the chord starts at the same time mark. The
+                     next event after the chord will be assigned that time mark
+                     + the duration of the longest note/rest in the chord.
+           * Voices: voices work just like chords. Each voice in the voices
+                     grouping starts at the same time mark. The next event after
+                     the voices grouping (whether via explicit V0: or switching
+                     back from another instrument) will start whenever the last
+                     voice has finished its music data... i.e. the 'largest' time
+                     marking out of all the voices.
 
-                     Implications: the composer will need to either make use of named markers
-                     or make sure, when switching from instrument to instrument, that he be
-                     aware that the end of the longest held voice is where the next event
-                     will come in after switching back to that instrument. I don't really
-                     foresee this as being an inconvience. Markers will be easy to use.
+                     Implications: the composer will need to either make use of
+                     named markers or make sure, when switching from instrument
+                     to instrument, that he be aware that the end of the longest
+                     held voice is where the next event will come in after
+                     switching back to that instrument. I don't really foresee
+                     this as being an inconvience. Markers will be easy to use.
 
-     -  At this point, will probably hand off the final parse trees (one per instrument)
-        to alda.sound_generator, which will hopefully be able to create audio segments
-        of each instrument at each time marking, and then use the time markings to layer all
-        the different audio segments together to create the final audio file.")
+     -  At this point, will probably hand off the final parse trees (one per
+        instrument) to alda.sound_generator, which will hopefully be able to
+        create audio segments of each instrument at each time marking, and then
+        use the time markings to layer all the different audio segments together
+        to create the final audio file.")
 
 
