510d3f3c4507642e3b77b2c09c2f02cadad28fda
Dave Yarwood <dave.yarwood@gmail.com>
2015-02-02 23:24:26 -0500

    bump deps, improve CLI arg parsing, add uberjar task

--- build.boot
+++ build.boot
@@ -1,10 +1,12 @@
+#!/usr/bin/env boot
+
 (set-env!
  :source-paths #{"src" "test"}
  :resource-paths #{"grammar"}
  :dependencies '[[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.2.4"]
+                 [org.clojure/tools.cli "0.3.1"]
                  [instaparse "1.3.5"]
-                 [adzerk/bootlaces "0.1.8" :scope "test"]
+                 [adzerk/bootlaces "0.1.9" :scope "test"]
                  [adzerk/boot-test "1.0.3" :scope "test"]])
 
 (require '[adzerk.bootlaces :refer :all]
@@ -14,14 +16,23 @@
 (bootlaces! +version+)
 
 (task-options!
+  aot {:namespace '#{alda.core}}
   pom {:project 'alda
        :version +version+
        :description "A music programming language for musicians"
-       :url "https://github.com/daveyarwood/alda"
-       :scm {:url "https://github.com/daveyarwood/alda"}
-       :license {:name "Eclipse Public License"
-                 :url "http://www.eclipse.org/legal/epl-v10.html"}}
+       :url "https://github.com/alda-lang/alda"
+       :scm {:url "https://github.com/alda-lang/alda"}
+       :license {"name" "Eclipse Public License"
+                 "url" "http://www.eclipse.org/legal/epl-v10.html"}}
+  jar {:main 'alda.core}
   test {:namespaces '#{alda.parser-test}})
 
+(deftask build
+  "Builds uberjar.
+   TODO: be able to build an executable à la lein bin"
+  []
+  (comp (aot) (pom) (uber) (jar)))
+
 (defn -main [& args]
-  "To do: refer to boot-uberjar example")
+  (require 'alda.core)
+  (apply (resolve 'alda.core/-main) args))
--- src/alda/core.clj
+++ src/alda/core.clj
@@ -1,7 +1,16 @@
 (ns alda.core
-  (:use [clojure.tools.cli :only (cli)])
-  (:require [alda.sound-generator :as soundgen])
-  (:gen-class :main true))
+  (:require [clojure.tools.cli :refer (parse-opts)]
+            [alda.sound-generator :as soundgen])
+  (:gen-class))
+
+(def cli-options
+  [["-h" "--help" "Display help text."]
+   ["-s" "--start START"
+    "Start reading the score at a specific minute/second mark or score marker."
+    :default "0:00"]
+    ; TODO: write :parse-fn and :validate functions (use for both start & end)
+   ["-e" "--end END"
+    "Stop playing the score at a specific minute/second mark or score marker."]])
 
 (defn dispatcher
   "If no output file is specified, plays the file using the specified options.
@@ -12,35 +21,27 @@
     (soundgen/make-wav input-file output-file opts)))
 
 (defn -main
-  "Parses command line arguments and dispatches the appropriate functions."
   [& args]
-  (let [[opts args banner]
-        (cli args
-         ["-h" "--help"
-               "Display help text."
-               :flag true, :default false]
-         ["-s" "--start"
-               "Start playing the score at a specific minute/second mark or score marker."
-               :default "0:00"]
-         ["-e" "--end"
-               "Stop playing at a specific minute/second mark or score marker."] ;; optional
-         )]
-    (when (:help opts)
-      (println banner)
-      (System/exit 0))
-    (try
-      (apply dispatcher (conj args opts))
-      (catch clojure.lang.ArityException e
-        (cond
-          (zero? (count args))
-          (println "Please specify an input file containing some alda code."
-                   "\n\n"
-                   "example:    alda chorale.alda")
-          (> (count args) 2)
-          (println "Invalid number of arguments. You must specify only one input file and"
-                   "(optionally) one output file."
-                   "\n\n"
-                   "example:    alda chorale.alda chorale.wav")))
-      (catch java.io.FileNotFoundException e
-        (let [bad-filename (first args)]
-          (println (format "Input file \"%s\" not found." bad-filename)))))))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options)
+      (do
+        (println summary)
+        (System/exit 0))
+      (zero? (count args))
+      (do
+        (println "Please specify an input file containing some alda code."
+                 "\n\nexample:    alda chorale.alda")
+        (System/exit 1))
+      (> (count args) 2)
+      (do
+        (println "Invalid number of arguments. You must specify only one input file and"
+                 "(optionally) one output file."
+                 "\n\nexample:    alda chorale.alda chorale.wav")
+        (System/exit 1))
+      :else
+      (try
+        (apply dispatcher (conj arguments options))
+        (catch java.io.FileNotFoundException e
+          (let [bad-filename (first arguments)]
+            (println (format "Input file \"%s\" not found." bad-filename))))))))
