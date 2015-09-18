8e52f819cbc23103dde84c8432f9bcd9194b4509
Dave Yarwood <dave.yarwood@gmail.com>
2015-09-18 13:35:37 -0400

    bugfix: actually include barline in alda.lisp

--- src/alda/lisp/events.clj
+++ src/alda/lisp/events.clj
@@ -2,5 +2,6 @@
   (:require [alda.lisp.events.note]
             [alda.lisp.events.rest]
             [alda.lisp.events.chord]
-            [alda.lisp.events.voice]))
+            [alda.lisp.events.voice]
+            [alda.lisp.events.barline]))
 (in-ns 'alda.lisp)
