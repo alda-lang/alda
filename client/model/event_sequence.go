426d92ec5315d1a7b80618688f059b587367b4cb
Dave Yarwood <dave.yarwood@gmail.com>
2019-05-25 22:19:16 -0400

    implement Score.Update; reimplement EventSequence.updateScore in terms of it

--- client/model/event_sequence.go
+++ client/model/event_sequence.go
@@ -5,11 +5,5 @@ type EventSequence struct {
 }
 
 func (es EventSequence) updateScore(score *Score) error {
-	for _, event := range es.Events {
-		if err := event.updateScore(score); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return score.Update(es.Events...)
 }
--- client/model/score.go
+++ client/model/score.go
@@ -6,6 +6,20 @@ package model
 // update aspects of the score data.
 type Score struct{}
 
+// Update applies a variable number of ScoreUpdates to a Score, short-circuiting
+// and returning the first error that occurs.
+//
+// Returns nil if no error occurs.
+func (score *Score) Update(updates ...ScoreUpdate) error {
+	for _, update := range updates {
+		if err := update.updateScore(score); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // The ScoreUpdate interface defines how something updates a score.
 type ScoreUpdate interface {
 	updateScore(score *Score) error
