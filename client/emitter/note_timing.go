3fc9e38fd6599d4affa0794644670bb9da57d2bc
Dave Yarwood <dave.yarwood@gmail.com>
2020-02-25 13:16:06 -0500

    rm track from the output because the order is non-deterministic in alda v1

--- client/emitter/note_timing.go
+++ client/emitter/note_timing.go
@@ -18,20 +18,17 @@ type NoteTimingEmitter struct{}
 // EmitScore implements Emitter.EmitScore by printing the offset, duration, and
 // pitch of every note in the score.
 func (nte NoteTimingEmitter) EmitScore(score *model.Score) error {
-	tracks := score.Tracks()
-
-	fmt.Println("track, offset, duration, midi note")
+	fmt.Println("offset, duration, midi note")
 
 	for _, event := range score.Events {
 		switch event.(type) {
 		case model.NoteEvent:
 			noteEvent := event.(model.NoteEvent)
 
-			track := tracks[noteEvent.Part]
 			offset := int32(math.Round(float64(noteEvent.Offset)))
 			duration := int32(math.Round(float64(noteEvent.AudibleDuration)))
 
-			fmt.Printf("%d, %d, %d, %d\n", track, offset, duration, noteEvent.MidiNote)
+			fmt.Printf("%d, %d, %d\n", offset, duration, noteEvent.MidiNote)
 		default:
 			return fmt.Errorf("unsupported event: %#v", event)
 		}
