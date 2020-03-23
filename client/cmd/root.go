4500c4b7d7cb23f175b46a82f279ce3673991ba0
Dave Yarwood <dave.yarwood@gmail.com>
2020-03-22 20:10:57 -0400

    improve log level setting / player spawning

--- client/cmd/root.go
+++ client/cmd/root.go
@@ -25,21 +25,29 @@ func init() {
 	}
 }
 
+func handleVerbosity(level int) error {
+	switch level {
+	case 0:
+		log.SetGlobalLevel("error")
+	case 1:
+		log.SetGlobalLevel("warn")
+	case 2:
+		log.SetGlobalLevel("info")
+	case 3:
+		log.SetGlobalLevel("debug")
+	default:
+		return fmt.Errorf("invalid verbosity level. Valid values are 0-3")
+	}
+
+	return nil
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "alda",
 	Short: "alda: a text-based language for music composition",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		switch verbosity {
-		case 0:
-			log.SetGlobalLevel("error")
-		case 1:
-			log.SetGlobalLevel("warn")
-		case 2:
-			log.SetGlobalLevel("info")
-		case 3:
-			log.SetGlobalLevel("debug")
-		default:
-			fmt.Println("Invalid verbosity level. Valid values are 0-3.")
+		if err := handleVerbosity(verbosity); err != nil {
+			fmt.Println(err)
 			os.Exit(1)
 		}
 	},
@@ -47,6 +55,63 @@ var rootCmd = &cobra.Command{
 
 // Execute parses command-line arguments and runs the Alda command-line client.
 func Execute() error {
+	// The default logging level is WARN.
+	level := 1
+
+	// Cobra is a little bit janky in that there doesn't seem to be a way to add a
+	// hook that will _always_ run regardless of the command. PersistentPreRun
+	// almost works, but it doesn't run in cases like:
+	//
+	//   alda
+	//   alda --help
+	//   alda some-bogus-command
+	//
+	// We always want to do two things:
+	//
+	//   1. Set the logging level based on the -v / --verbosity option.
+	//   2. Spawn player processes.
+	//
+	// It's especially important that we do this when the user first installs Alda
+	// and they run `alda` or `alda --help` to explore the commands and options.
+	// By the time they figure out how to craft an `alda play ...` command, a
+	// player process will have fully spawned in the background and they'll hear
+	// output immediately, which is a great user experience.
+	//
+	// As a hacky workaround, we scan through the command line arguments here,
+	// make an attempt to set the log level, and spawn player processes, before we
+	// invoke Cobra and handle CLI arguments properly.
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+
+		switch arg {
+		case "-v0", "-v=0", "--verbosity=0":
+			level = 0
+		case "-v1", "-v=1", "--verbosity=1":
+			level = 1
+		case "-v2", "-v=2", "--verbosity=2":
+			level = 2
+		case "-v3", "-v=3", "--verbosity=3":
+			level = 3
+		case "-v", "--verbosity":
+			i++
+			switch os.Args[i] {
+			case "0":
+				level = 0
+			case "1":
+				level = 1
+			case "2":
+				level = 2
+			case "3":
+				level = 3
+			}
+		}
+
+		if err := handleVerbosity(level); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
 	// Regardless of the command being run*, Alda will preemptively spawn player
 	// processes in the background, up to a desired amount. This helps to ensure
 	// that the application will feel fast, because each time you need a player
