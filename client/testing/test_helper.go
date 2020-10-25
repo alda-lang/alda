59fb704b18a0092f553664c40ab344500c3a99e1
Dave Yarwood <dave.yarwood@gmail.com>
2020-10-25 09:19:20 -0400

    ensure that the Go testing package is initiated before the Alda one
    This hopefully fixes the issue we were seeing in CI, related to
    https://github.com/golang/go/issues/31859.

--- client/testing/test_helper.go
+++ client/testing/test_helper.go
@@ -1,8 +1,19 @@
 package testing
 
 import (
-	log "alda.io/client/logging"
 	"flag"
+	// Apparently there was a breaking change in Go 1.13 related to the order that
+	// flags are parsed in init functions, or something. When I tried upgrading
+	// the Go version we're using in CI from 1.12.9 to 1.15.3, I got the same
+	// error message as the one reported in this issue:
+	//
+	//   https://github.com/golang/go/issues/31859
+	//
+	// Someone pointed out that explicitly initiating the testing package would
+	// fix the issue, so that's what this import is doing.
+	_ "testing"
+
+	log "alda.io/client/logging"
 )
 
 func init() {
