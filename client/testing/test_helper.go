9241c2cbc933175cbc7ceb299d55937659690804
Dave Yarwood <dave.yarwood@gmail.com>
2020-10-25 20:39:01 -0400

    adjust comment now that i'm doing more than importing "testing"

--- client/testing/test_helper.go
+++ client/testing/test_helper.go
@@ -2,6 +2,12 @@ package testing
 
 import (
 	"flag"
+	"testing"
+
+	log "alda.io/client/logging"
+)
+
+func init() {
 	// Apparently there was a breaking change in Go 1.13 related to the order that
 	// flags are parsed in init functions, or something. When I tried upgrading
 	// the Go version we're using in CI from 1.12.9 to 1.15.3, I got the same
@@ -10,13 +16,7 @@ import (
 	//   https://github.com/golang/go/issues/31859
 	//
 	// Someone pointed out that explicitly initiating the testing package would
-	// fix the issue, so that's what this import is doing.
-	"testing"
-
-	log "alda.io/client/logging"
-)
-
-func init() {
+	// fix the issue, so that's what this is doing.
 	testing.Init()
 
 	level := flag.String("log-level", "", "Logging level")
