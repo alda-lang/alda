0040c8f959d4d41b165f7166d372f0030e90c8f7
David Lu <david.scowluga@gmail.com>
2021-07-14 17:32:43 -0400

    Document Format method

--- client/code_formatter/formatter.go
+++ client/code_formatter/formatter.go
@@ -6,6 +6,9 @@ import (
 	"io"
 )
 
+// Format formats Alda token text into readable code
+// Format can be used as the last step in the decompiler process, or by itself
+// as a standalone formatting command 
 func Format(tokens []parser.Token, w io.Writer) {
 	for _, token := range tokens {
 		fmt.Fprint(w, token.Text)
