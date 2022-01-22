d0d4a0cc4520f10093976321a80eb982928c88c4
Dave Yarwood <dave.yarwood@gmail.com>
2022-01-22 11:07:30 -0500

    Add a function to set the log level

--- player/src/jsMain/kotlin/Main.kt
+++ player/src/jsMain/kotlin/Main.kt
@@ -2,6 +2,8 @@ package io.alda.player
 
 import kotlinx.browser.window
 import kotlin.js.Json
+import mu.KotlinLoggingConfiguration
+import mu.KotlinLoggingLevel
 
 // It's absurd that Kotlin.js doesn't provide a stdlib function for this!
 // Source: https://stackoverflow.com/a/47380123/2338327
@@ -11,12 +13,22 @@ inline fun jsObject(init: dynamic.() -> Unit): Json {
     return o
 }
 
+fun setLogLevel(level : String) {
+  KotlinLoggingConfiguration.LOG_LEVEL = when (level) {
+    "trace" -> KotlinLoggingLevel.TRACE
+    "debug" -> KotlinLoggingLevel.DEBUG
+    "info"  -> KotlinLoggingLevel.INFO
+    "warn"  -> KotlinLoggingLevel.WARN
+    "error" -> KotlinLoggingLevel.ERROR
+    else    -> throw IllegalArgumentException("Invalid log level: ${level}")
+  }
+}
+
 fun main() {
   console.info("alda-player.js version ${playerVersion} loaded")
 
   window.asDynamic().AldaPlayer = jsObject {
-    aldaJsTestFunction = fun (n: Int) : String {
-      return "aldaJsTestFunction output for n: ${n}"
+    setLogLevel = ::setLogLevel
     }
   }
 }
