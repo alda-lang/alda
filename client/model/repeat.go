8e07c1cb56380a44ab488bb1c4d344f1b1744e74
De-Alchmst <PolakJ_37@proton.me>
2025-05-19 19:28:24 +0200

    issue fixed by resetting count at the end of the loop

--- client/model/repeat.go
+++ client/model/repeat.go
@@ -31,13 +31,20 @@ func (repeat Repeat) JSON() *json.Container {
 // UpdateScore implements ScoreUpdate.UpdateScore by repeatedly updating the
 // score with an event a specified number of times.
 func (repeat Repeat) UpdateScore(score *Score) error {
+	previousRepetitions := make([]int32, len(score.CurrentParts))
+
 	for repetition := int32(1); repetition <= repeat.Times; repetition++ {
-		for _, part := range score.CurrentParts {
+		for i, part := range score.CurrentParts {
+			previousRepetitions[i] = part.currentRepetition
 			part.currentRepetition = repetition
 		}
 
 		if err := score.Update(repeat.Event); err != nil {
 			return err
+		}
+
+		for i, part := range score.CurrentParts {
+			part.currentRepetition = previousRepetitions[i]
 		}
 	}
 
@@ -50,12 +57,17 @@ func (repeat Repeat) DurationMs(part *Part) float64 {
 	durationMs := 0.0
 
 	for repetition := int32(1); repetition <= repeat.Times; repetition++ {
+		previousRepetition := part.currentRepetition
 		part.currentRepetition = repetition
+
 		durationMs += repeat.Event.DurationMs(part)
+
+		part.currentRepetition = previousRepetition
 	}
 
 	return durationMs
 }
+
 
 // VariableValue implements ScoreUpdate.VariableValue by returning a version of
 // the repeat where the value of the event to be repeated is captured.
